a1fb2af7d47abe7f4fa861d7b7b3acd486082936
Michael Evans <michaeljevans113@gmail.com>
2020-08-13 20:28:29 -0600

    Add Asteroid class

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -12,27 +12,33 @@ class NearEarthObjects
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
-    asteroids = conn.get('/neo/rest/v1/feed')
+    raw_asteroids_data = conn.get('/neo/rest/v1/feed')
 
-    parsed_asteroids_data = JSON.parse(asteroids.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+    parsed_asteroids_data = JSON.parse(raw_asteroids_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
 
     largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
       asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a <=> b}
 
     total_number_of_asteroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
-      {
-        name: asteroid[:name],
-        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
+
+    asteroids = parsed_asteroids_data.map do |asteroid_data|
+      Asteroid.new(asteroid_data)
     end
 
     {
-      asteroid_list: formatted_asteroid_data,
+      asteroid_list: asteroids,
       biggest_asteroid: largest_asteroid_diameter,
       total_number_of_asteroids: total_number_of_asteroids
     }
+  end
+end
+
+class Asteroid
+  attr_reader :name, :diameter, :miss_distance
+  def initialize(asteroid_data)
+    @name          = asteroid_data[:name]
+    @diameter      = "#{asteroid_data[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft"
+    @miss_distance = "#{asteroid_data[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
   end
 end
