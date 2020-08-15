5eb15816f21f749ace353bcde6bfe38173c1086d
Michael Evans <michaeljevans113@gmail.com>
2020-08-15 16:57:46 -0600

    Current progress

---
+++ gui.rb
@@ -0,0 +1,14 @@
+class GUI
+
+  def print_intro
+    puts "________________________________________________________________________________________________________________________________"
+    puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+    puts "Please enter a date in the following format YYYY-MM-DD."
+    print ">> "
+  end
+
+  def method_name
+
+  end
+
+end
