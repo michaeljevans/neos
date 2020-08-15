4fd57bb1910ab495b032203c6154e8485ab2f614
Michael Evans <michaeljevans113@gmail.com>
2020-08-15 16:57:35 -0600

    Create helper methods and extract unrelated code to other classes

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,44 +1,33 @@
-require 'faraday'
 require 'figaro'
-require 'json'
-require 'pry'
-# Load ENV vars via Figaro
+require_relative 'neo_service'
+require_relative 'asteroid'
+
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
 
 class NearEarthObjects
-  def self.find_neos_by_date(date)
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
-    raw_asteroids_data = conn.get('/neo/rest/v1/feed')
 
-    parsed_asteroids_data = JSON.parse(raw_asteroids_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
-    largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
-      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    end.max { |a,b| a <=> b}
-
-    total_number_of_asteroids = parsed_asteroids_data.count
-
-    asteroids = parsed_asteroids_data.map do |asteroid_data|
-      Asteroid.new(asteroid_data)
-    end
+  def find_neos_by_date(date)
+    service = NEOService.new
+    neo_data = service.get_neo_data(date)
 
     {
-      asteroid_list: asteroids,
-      biggest_asteroid: largest_asteroid_diameter,
-      total_number_of_asteroids: total_number_of_asteroids
+      asteroid_list: asteroids(neo_data),
+      biggest_asteroid: largest_asteroid_diameter(neo_data),
+      total_number_of_asteroids: asteroid_count(neo_data)
     }
   end
-end
 
-class Asteroid
-  attr_reader :name, :diameter, :miss_distance
-  def initialize(asteroid_data)
-    @name          = asteroid_data[:name]
-    @diameter      = "#{asteroid_data[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft"
-    @miss_distance = "#{asteroid_data[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+  def asteroids(neo_data)
+    neo_data.map { |asteroid_data| Asteroid.new(asteroid_data) }
   end
+
+  def largest_asteroid_diameter(neo_data)
+    neo_data.map { |asteroid| asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i }.max
+  end
+
+  def asteroid_count(neo_data)
+    neo_data.count
+  end
+
 end
